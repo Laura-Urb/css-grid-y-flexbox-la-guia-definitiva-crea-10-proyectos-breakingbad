35d8843eaab20973bf8aac07cd0f15859b07459b
Laura Urb <lauraiurb@gmail.com>
2021-06-07 11:07:40 -0300

    First commit

---
+++ docs/precache-manifest.2cb8a682db4daf1dfe84a6e4184c48e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30ed8d5eb529003ec6b024f35baa83b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1e40f2e12fe6be73760",
+    "url": "/static/css/main.f9403ed5.chunk.css"
+  },
+  {
+    "revision": "75b6f063ac31a8061a29",
+    "url": "/static/js/2.fb11082c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fb11082c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e40f2e12fe6be73760",
+    "url": "/static/js/main.68ca0f67.chunk.js"
+  },
+  {
+    "revision": "bfc935588e886ab68f5b",
+    "url": "/static/js/runtime-main.833978df.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/static/media/logo.427a4adc.svg"
+  }
+]);
